e282e363066fba9a13561ebcd13070ea9c311de6
ramsaym <48220108+ramsaym@users.noreply.github.com>
2023-03-15 10:29:59 -0300

    Second feedback test

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, World! Hello Moon"
+    return "Hello, World! Hello Moon, Hellow Jupiter."
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
